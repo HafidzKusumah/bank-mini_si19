61608f65574d20aa3ec4fd7f7c7ad49fe432d98e
rifaaraihan <rifa.raihan88@gmail.com>
2021-10-03 21:34:22 +0700

    menambahkan index.php

--- js/index.php
+++ js/index.php
@@ -20,7 +20,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Genoale | Online Watch Shop</title>
+		<title>Bank Mini | Website </title>
 		
 		<link href="<?php echo BASE_URL."css/style.css"; ?>" type="text/css" rel="stylesheet" />
 			
